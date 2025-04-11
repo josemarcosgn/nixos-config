6e89505333ac83ca6eae93e7c3e6bc1db6ad1190
Jose Marcos <josemarcos.tecno@gmail.com>
2025-04-11 01:18:20 -0300

    remove cuda

--- hosts/laptop-x550l/configuration.nix
+++ hosts/laptop-x550l/configuration.nix
@@ -155,7 +155,6 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    cuda
   ];
 
   system.stateVersion = "24.11"; # Did you read the comment?
