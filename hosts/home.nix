4815455a3404bab4bf59d8ee910a67c230e243b2
Jose Marcos <josemarcos.tecno@gmail.com>
2025-04-11 01:33:07 -0300

    Remove unnecessary comments and update ZSH with Oh My Zsh configuration
    - Removed unused or redundant comments for a cleaner configuration.
    - Configured ZSH with Oh My Zsh, enabled the "agnoster" theme, and added plugins (git, sudo, z).
    - Enabled ZSH features like completion, autosuggestions, and syntax highlighting.

--- hosts/home.nix
+++ hosts/home.nix
@@ -1,59 +1,19 @@
 { config, pkgs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
+
   home.username = "jose";
   home.homeDirectory = "/home/jose";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
+  home.stateVersion = "24.11";
 
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
-  
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/jose/etc/profile.d/hm-session-vars.sh
-  #
   home.sessionVariables = {
-    # EDITOR = "gnome-text-editor";
+    EDITOR = "codium";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
   
   home.packages = with pkgs.gnomeExtensions; [
@@ -111,4 +71,21 @@
       ms-ceintl.vscode-language-pack-pt-br
     ];
   };
+  
+  programs.zsh = {
+    enable = true;
+    ohMyZsh = {
+      enable = true;
+      theme = "agnoster"; # You can change the theme here
+      plugins = [ "git" "sudo" "z" ];
+    };
+  };
+
+  # Set zsh as the default shell in Home Manager
+  programs.zsh.enableCompletion = true;
+  programs.zsh.enableAutosuggestions = true;
+  programs.zsh.enableSyntaxHighlighting = true;
+
+  # Optional: if you haven't set the default shell yet
+  home.shell = pkgs.zsh;
 }
