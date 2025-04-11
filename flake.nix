d25030eca8820f3b66105fc33b945ff5ce295e2b
Jose Marcos <josemarcos.tecno@gmail.com>
2025-04-10 21:44:00 -0400

    fix(flake.nix): remove duplicated outputs block and invalid home-manager reference
    This commit fixes a syntax error caused by a duplicated 'outputs' block in flake.nix.
    It also removes an outdated reference to home-manager in the flake outputs, since home-manager is now configured only within each host module.

--- flake.nix
+++ flake.nix
@@ -10,21 +10,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
-  outputs = { self, nixpkgs, ... }@inputs: {
-    # use "nixos", or your hostname as the name of the configuration
-    # it's a better practice than "default" shown in the video
-    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
-      specialArgs = {inherit inputs;};
-      modules = [
-        ./hosts/desktop-x99/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
-    };
-  };
-}
-
-
   outputs = { self, nixpkgs, ... }@inputs: {
     nixosConfigurations = {
       desktop = nixpkgs.lib.nixosSystem {
